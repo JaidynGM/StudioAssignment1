1a626398f259fed65f121cc1688fb5603dcdefe9
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2025-01-20 17:53:23 -0800

    Setup the environment

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,40 +2,12 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public Rigidbody sphereRigidBody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] float ballSpeed;
 
-    void Start()
+    public void MoveBall(Vector2 input)
     {
-        
-    }
-
-    void Update()
-    {
-        Vector2 inputVector = Vector2.zero; 
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector += Vector2.up;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector += Vector2.left;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector += Vector2.down;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector += Vector2.right;
-        }
-
-        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
-        sphereRigidBody.AddForce(inputXZPlane * ballSpeed);
-        Debug.Log("Resultant 3D Vector: " + inputXZPlane);
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 }
