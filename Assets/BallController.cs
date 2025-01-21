5db12802382f6b1e52ae0f064565ea108147868c
Jaidyn Gordon-Mason <jaidynrocks01@gmail.com>
2025-01-20 18:21:05 -0800

    Added jump mechanics

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -3,11 +3,28 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
-    [SerializeField] float ballSpeed;
+    [SerializeField] private float ballSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform groundCheck;
+    private float groundCheckRadius = 0.5f;
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+    }
 
     public void MoveBall(Vector2 input)
     {
-        Vector3 inputXZPlane = new(input.x, 0, input.y);
+        Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+    }
+
+    public void Jump()
+    {
+        if (IsGrounded())
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+    public UnityEvent OnJump = new UnityEvent();
 
     void Update()
     {
@@ -30,5 +31,10 @@ public class InputManager : MonoBehaviour
         }
 
         OnMove?.Invoke(inputVector);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJump?.Invoke();
+        }
     }
 }
